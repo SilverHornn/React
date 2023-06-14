03ebfa3676d2a9a29f2c68323b9e36c3c10e9c93
DCQDuy <anhkuduy1603@gmail.com>
2023-06-14 11:19:46 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import React, {Component} from "react";
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
